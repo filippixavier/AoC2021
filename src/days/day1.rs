a82966a185462e84eb00d2ed3cd95194761f761f
Xavier FILIPPI <xavier.filippi.prestataire@sfr.com>
2021-12-01 11:25:59 +0100

    Day 1 Star 2

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -2,12 +2,17 @@ use std::error::Error;
 use std::fs;
 use std::path::Path;
 
-pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
-    let depths: Vec<usize> = fs::read_to_string(Path::new("./data/day1.input"))?
+fn get_input() -> Vec<usize> {
+    fs::read_to_string(Path::new("./data/day1.input"))
+        .unwrap_or(String::from(""))
         .trim()
         .split('\n')
         .map(|depth| (depth.trim().parse::<usize>().unwrap_or(0)))
-        .collect();
+        .collect()
+}
+
+pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
+    let depths = get_input();
 
     let mut count = 0;
 
@@ -17,11 +22,27 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
         }
     }
 
-    println!("{}", count);
+    println!("Number of depth increase: {}", count);
 
     Ok(())
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    let depths = get_input();
+    let mut count = 0;
+    let mut previous = 0;
+
+    for i in 0..depths.len() - 2 {
+        let sum = depths[i] + depths[i + 1] + depths[i + 2];
+        if previous != 0 {
+            if previous < sum {
+                count += 1;
+            }
+        }
+        previous = sum;
+    }
+
+    println!("Number of depth increase by group of three is: {}", count);
+
     Ok(())
 }
