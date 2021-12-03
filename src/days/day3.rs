f800358fb9992a19a16eb1b5811c5cb965b6bfbc
Xavier FILIPPI <xavier.filippi.prestataire@sfr.com>
2021-12-03 13:21:02 +0100

    Day 3 Star 2

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -47,6 +47,54 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
     Ok(())
 }
 
+fn reduce_common(mut reading: Vec<String>, line_len: usize, use_common: bool) -> String {
+    let mut index = 0;
+    while reading.len() != 1 {
+        let mut one_count: Vec<usize> = vec![0; line_len];
+        for line in reading.iter() {
+            for (i, val) in line.chars().enumerate() {
+                if val == '1' {
+                    one_count[i] += 1;
+                }
+            }
+        }
+
+        let denominator: Vec<char> = one_count
+            .into_iter()
+            .map(|l| if l * 2 >= reading.len() { '1' } else { '0' })
+            .collect();
+        reading = reading
+            .clone()
+            .into_iter()
+            .filter(|line| {
+                let den = denominator[index];
+                let ch = line.chars().nth(index).unwrap();
+                if use_common {
+                    ch == den
+                } else {
+                    ch != den
+                }
+            })
+            .collect();
+        index += 1;
+    }
+    reading[0].clone()
+}
+
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    let (reading, line_len) = get_input();
+
+    let o2_reading =
+        isize::from_str_radix(reduce_common(reading.clone(), line_len, true).as_str(), 2).unwrap();
+    let co2_reading =
+        isize::from_str_radix(reduce_common(reading, line_len, false).as_str(), 2).unwrap();
+
+    println!(
+        "O² reading: {}, CO² reading: {}, life support rating: {}",
+        o2_reading,
+        co2_reading,
+        o2_reading * co2_reading
+    );
+
     Ok(())
 }
