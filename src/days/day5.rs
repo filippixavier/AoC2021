9e5f58657a210a0b0141dc06dd0b10a47782ac3d
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2021-12-05 16:05:39 +0100

    Day 5 Star 2

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -2,16 +2,17 @@ use std::error::Error;
 use std::fs;
 use std::path::Path;
 
-type Point = (u32, u32);
-type Segment = Vec<Point>;
+use std::collections::HashMap;
 
-fn get_input() -> Vec<Segment> {
+type Point = (u32, u32);
+
+fn get_input2(with_diag: bool) -> HashMap<Point, u8> {
+    let mut map = HashMap::new();
+
     let input = fs::read_to_string(Path::new("./data/day5.input"))
         .expect("Something went wrong when reading input");
-    let mut vents = vec![];
-    for line in input.trim().lines() {
-        let mut segment = vec![];
 
+    for line in input.trim().lines() {
         let coordinates: Vec<&str> = line.trim().split_ascii_whitespace().collect();
 
         let inter1: Vec<u32> = coordinates[0]
@@ -40,7 +41,9 @@ fn get_input() -> Vec<Segment> {
                 end = p1;
             }
             for i in start.1..=end.1 {
-                segment.push((start.0, i));
+                let position = (start.0, i);
+                let count = map.entry(position).or_insert(0);
+                *count += 1;
             }
         } else if p1.1 == p2.1 {
             if p1.0 < p2.0 {
@@ -52,38 +55,48 @@ fn get_input() -> Vec<Segment> {
             }
 
             for i in start.0..=end.0 {
-                segment.push((i, start.1));
+                let position = (i, start.1);
+                let count = map.entry(position).or_insert(0);
+                *count += 1;
+            }
+        } else if with_diag {
+            if p1.0 < p2.0 {
+                start = p1;
+                end = p2;
+            } else {
+                start = p2;
+                end = p1;
+            }
+
+            for (step, i) in (start.0..=end.0).into_iter().enumerate() {
+                let position = if start.1 < end.1 {
+                    (i, start.1 + step as u32)
+                } else {
+                    (i, start.1 - step as u32)
+                };
+                let count = map.entry(position).or_insert(0);
+                *count += 1;
             }
         }
-
-        if !segment.is_empty() {
-            vents.push(segment);
-        }
     }
-    vents
+
+    map
 }
 
 pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
-    let mut segments = get_input();
-    let mut intersections: Vec<Point> = vec![];
+    let map = get_input2(false);
 
-    while segments.len() > 1 {
-        let current = segments.pop().unwrap();
+    let count = map.values().filter(|i| **i > 1).count();
 
-        for segment in segments.iter() {
-            let intersection = segment.iter().filter(|p| current.contains(p));
-            for inter in intersection {
-                if !intersections.contains(inter) {
-                    intersections.push(*inter);
-                }
-            }
-        }
-    }
-
-    println!("Number of overlapping points: {}", intersections.len());
+    println!("Number of overlapping points: {}", count);
     Ok(())
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    let map = get_input2(true);
+
+    let count = map.values().filter(|i| **i > 1).count();
+
+    println!("Number of overlapping points (with diag): {}", count);
     Ok(())
 }
