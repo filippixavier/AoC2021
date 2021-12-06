88296bb21527b7e3879480df27afcbec81193c50
Xavier FILIPPI <xavier.filippi.prestataire@sfr.com>
2021-12-06 10:45:03 +0100

    Day 6 Star 2

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -20,21 +20,29 @@ fn get_school() -> [usize; 9] {
     school
 }
 
-pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
-    let mut school: VecDeque<usize> = get_school().into_iter().collect();
-
-    for _ in 0..80 {
+fn get_total_after_days(mut school: VecDeque<usize>, days: usize) -> usize {
+    for _ in 0..days {
         let temp = school.pop_front().unwrap();
         let reset = school.get_mut(6).unwrap();
         *reset += temp;
         school.push_back(temp);
     }
 
-    println!("{}", school.iter().sum::<usize>());
+    school.iter().sum::<usize>()
+}
+
+pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
+    let school: VecDeque<usize> = get_school().into_iter().collect();
+
+    println!("{}", get_total_after_days(school, 80));
 
     Ok(())
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    let school: VecDeque<usize> = get_school().into_iter().collect();
+
+    println!("{}", get_total_after_days(school, 256));
+
     Ok(())
 }
