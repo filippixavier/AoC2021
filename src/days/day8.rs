dd148159ad9aaefa3749c2b1640759ddd8b3eb4d
Xavier FILIPPI <xavier.filippi.prestataire@sfr.com>
2021-12-08 15:52:34 +0100

    Day 8 Star 2

--- src/days/day8.rs
+++ src/days/day8.rs
@@ -33,7 +33,7 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
         count += display
             .iter()
             .filter(|sig| {
-                let len = sig.chars().count();
+                let len = sig.len();
                 len == 2 || len == 3 || len == 4 || len == 7
             })
             .count();
@@ -44,6 +44,96 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
     Ok(())
 }
 
+fn reorder(input: &[String]) -> Vec<String> {
+    input
+        .iter()
+        .map(|l| {
+            let mut x = l.chars().collect::<Vec<_>>();
+            x.sort_unstable();
+            x.into_iter().collect::<String>()
+        })
+        .collect::<Vec<_>>()
+}
+
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    use std::collections::HashMap;
+
+    let input = get_input();
+    let mut count = 0;
+
+    for (signals, display) in input {
+        let mut coupling: HashMap<usize, String> = HashMap::new();
+        let mut sig = reorder(&signals);
+        let dis = reorder(&display);
+
+        // Find 1, 4, 7, 8
+        sig = sig
+            .into_iter()
+            .filter(|x| match x.len() {
+                2 => {
+                    coupling.insert(1, x.clone());
+                    false
+                }
+                3 => {
+                    coupling.insert(7, x.clone());
+                    false
+                }
+                4 => {
+                    coupling.insert(4, x.clone());
+                    false
+                }
+                7 => {
+                    coupling.insert(8, x.clone());
+                    false
+                }
+                _ => true,
+            })
+            .collect();
+        // Find 2, 3, 5
+        sig = sig
+            .into_iter()
+            .filter(|x| match x.len() {
+                5 => {
+                    let one = coupling.get(&1).unwrap().chars();
+                    let four = coupling.get(&4).unwrap().chars();
+                    let count = one.filter(|y| !x.contains(*y)).count();
+                    if count == 0 {
+                        coupling.insert(3, x.clone());
+                    } else if four.filter(|y| !x.contains(*y)).count() == 2 {
+                        coupling.insert(2, x.clone());
+                    } else {
+                        coupling.insert(5, x.clone());
+                    }
+                    false
+                }
+                _ => true,
+            })
+            .collect();
+        // find 0, 6, 9
+        sig.into_iter().for_each(|x| {
+            if x.len() == 6 {
+                let one = coupling.get(&1).unwrap().chars();
+                let three = coupling.get(&3).unwrap().chars();
+                let count = three.filter(|y| !x.contains(*y)).count();
+                if count == 0 {
+                    coupling.insert(9, x);
+                } else if one.filter(|y| !x.contains(*y)).count() == 0 {
+                    coupling.insert(0, x);
+                } else {
+                    coupling.insert(6, x);
+                }
+            }
+        });
+
+        let coupling: HashMap<String, usize> = coupling.into_iter().map(|(k, v)| (v, k)).collect();
+
+        count += dis.iter().fold(0, |acc, value| {
+            let temp = coupling.get(value).unwrap_or(&0);
+            acc * 10 + temp
+        });
+    }
+
+    println!("Total output: {}", count);
+
     Ok(())
 }
