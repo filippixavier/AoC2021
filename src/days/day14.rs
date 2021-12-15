9253ecd56f6968481c61b835fb91281d7564fcba
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2021-12-15 10:06:19 +0100

    Day 14 Star 2

--- src/days/day14.rs
+++ src/days/day14.rs
@@ -3,17 +3,19 @@ use std::error::Error;
 
 const INPUT: &str = include_str!("../../data/day14.input");
 
-type PairInsertion = HashMap<String, String>;
+type Pair = (char, char);
+type PairInsertion = HashMap<Pair, char>;
 
-fn get_input() -> (String, PairInsertion) {
+fn get_input() -> (Vec<char>, PairInsertion) {
     let mut lines = INPUT.trim().lines();
     let mut pair_insertion = PairInsertion::new();
 
-    let template = lines.next().unwrap().to_string();
+    let template = lines.next().unwrap().chars().collect();
 
     for line in lines.skip(1) {
-        let parts = line.split(" -> ").collect::<Vec<_>>();
-        pair_insertion.insert(parts[0].to_string(), parts[1].to_string());
+        let parts: Vec<_> = line.split(" -> ").collect();
+        let input: Vec<_> = parts[0].chars().collect();
+        pair_insertion.insert((input[0], input[1]), parts[1].chars().next().unwrap());
     }
 
     (template, pair_insertion)
@@ -24,23 +26,22 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
     let mut counters: HashMap<char, usize> = HashMap::new();
 
     for _ in 0..10 {
-        let mut next_phase = String::new();
-        let chars = template.chars().collect::<Vec<_>>();
+        let mut next_phase = vec![];
 
-        for i in 0..chars.len() - 1 {
-            let current = chars[i].to_string() + &chars[i + 1].to_string();
-            next_phase += &chars[i].to_string();
+        for i in 0..template.len() - 1 {
+            let current = (template[i], template[i + 1]);
+            next_phase.push(template[i]);
             if let Some(insert) = pair_insertion.get(&current) {
-                next_phase += &insert.to_string();
+                next_phase.push(*insert);
             }
         }
 
-        next_phase += &chars.last().unwrap().to_string();
+        next_phase.push(*template.last().unwrap());
 
         template = next_phase;
     }
 
-    for i in template.chars() {
+    for i in template {
         let count = counters.entry(i).or_insert(0);
         *count += 1;
     }
@@ -54,5 +55,50 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    let (template, pair_insertion) = get_input();
+    let mut counters: HashMap<char, u64> = HashMap::new();
+
+    let mut pair_count: HashMap<Pair, u64> = pair_insertion.keys().map(|&key| (key, 0)).collect();
+
+    for i in 0..template.len() {
+        let elem = template[i];
+        let count = counters.entry(elem).or_insert(0);
+        *count += 1;
+
+        if i < template.len() - 1 {
+            let pair = (elem, template[i + 1]);
+            if pair_insertion.contains_key(&pair) {
+                let count = pair_count.entry(pair).or_insert(0);
+                *count += 1;
+            }
+        }
+    }
+
+    for _ in 0..40 {
+        let mut next_pair = HashMap::new();
+
+        for (pair, amount) in pair_count {
+            let insert = *pair_insertion.get(&pair).unwrap();
+            let count = counters.entry(insert).or_insert(0);
+            *count += amount;
+
+            let byproducts = [(pair.0, insert), (insert, pair.1)];
+
+            for byproduct in byproducts {
+                if pair_insertion.contains_key(&byproduct) {
+                    let count = next_pair.entry(byproduct).or_insert(0);
+                    *count += amount;
+                }
+            }
+        }
+
+        pair_count = next_pair;
+    }
+
+    let min = counters.values().min().unwrap();
+    let max = counters.values().max().unwrap();
+
+    println!("Big Element score is: {}", max - min);
+
     Ok(())
 }
