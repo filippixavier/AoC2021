cd82832a6f007bba5f301b6da3232becf1740489
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2021-12-26 02:11:52 +0100

    Optimize Day 23

--- src/days/day23.rs
+++ src/days/day23.rs
@@ -1,222 +1,177 @@
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::error::Error;
 
 static INPUT: &str = include_str!("../../data/day23.input");
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Tiles {
+type Coordinates = (usize, usize);
+type Map = HashMap<Coordinates, Tile>;
+
+enum Tile {
     Void,
+    Wall,
     Empty,
     Forbidden,
-    Room,
-    Wall,
-    Amphi(char),
+    Room(char),
 }
 
-use Tiles::*;
+use Tile::*;
 
-type Coordinates = (usize, usize);
-
-type Map = HashMap<Coordinates, Tiles>;
-
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 struct Amphipod {
-    sub_type: char,
-    coordinates: Coordinates,
-    targets: [Coordinates; 2],
-    tile_type: Tiles,
+    position: Coordinates,
     energy: usize,
+    species: char,
+    id: usize,
 }
-
-static AMBER: Amphipod = Amphipod {
-    sub_type: 'A',
-    coordinates: (0, 0),
-    targets: [(3, 2), (3, 3)],
-    tile_type: Room,
-    energy: 1,
-};
-
-static BRONZE: Amphipod = Amphipod {
-    sub_type: 'B',
-    coordinates: (0, 0),
-    targets: [(5, 2), (5, 3)],
-    tile_type: Room,
-    energy: 10,
-};
-
-static COPPER: Amphipod = Amphipod {
-    sub_type: 'C',
-    coordinates: (0, 0),
-    targets: [(7, 2), (7, 3)],
-    tile_type: Room,
-    energy: 100,
-};
-
-static DESERT: Amphipod = Amphipod {
-    sub_type: 'D',
-    coordinates: (0, 0),
-    targets: [(9, 2), (9, 3)],
-    tile_type: Room,
-    energy: 1000,
-};
 
 impl Amphipod {
-    fn new(sub_type: char, coordinates: Coordinates) -> Self {
-        let mut amphi = match sub_type {
-            'A' => AMBER.clone(),
-            'B' => BRONZE.clone(),
-            'C' => COPPER.clone(),
-            'D' => DESERT.clone(),
+    fn new(position: Coordinates, species: char, id: usize) -> Self {
+        let energy = match species {
+            'A' => 1,
+            'B' => 10,
+            'C' => 100,
+            'D' => 1000,
             _ => unreachable!(),
         };
-        amphi.coordinates = coordinates;
-        amphi
+        Amphipod {
+            position,
+            energy,
+            species,
+            id,
+        }
     }
 
-    fn get_move(&self, map: &Map) -> bool {
-        let mut need = true;
-        if self.targets[1] == self.coordinates {
-            need = false;
+    fn need_move(&self, map: &Map, amphibs: &[Amphipod]) -> bool {
+        if let Room(t) = map.get(&self.position).unwrap() {
+            if *t == self.species {
+                Amphipod::have_misplaced(self.position, self.species, map, amphibs)
+            } else {
+                true
+            }
+        } else {
+            true
         }
-        if self.targets[0] == self.coordinates {
-            if let Amphi(x) = map.get(&self.targets[1]).unwrap() {
-                if *x == self.sub_type {
-                    need = false;
+    }
+
+    fn have_misplaced(
+        coordinate: Coordinates,
+        species: char,
+        map: &Map,
+        amphibs: &[Amphipod],
+    ) -> bool {
+        let mut y = coordinate.1 + 1;
+        let mut misplaced = false;
+        'outer: loop {
+            if let Wall = map.get(&(coordinate.0, y)).unwrap() {
+                break;
+            }
+            for amphi in amphibs {
+                if amphi.position == (coordinate.0, y) && amphi.species != species {
+                    misplaced = true;
+                    break 'outer;
                 }
             }
+            y += 1;
         }
-        need
+        misplaced
     }
 
-    fn try_move_out(&self, map: &Map) -> Option<Vec<(Coordinates, usize)>> {
-        let mut visited: HashSet<Coordinates> = vec![self.coordinates].into_iter().collect();
-        let neighbors = [(-1, 0), (0, -1), (1, 0), (0, 1)];
-        let mut current_coordinates = vec![(self.coordinates, 0)];
-        let mut candidates = vec![];
-        while !current_coordinates.is_empty() {
-            let (coordinates, steps) = current_coordinates.pop().unwrap();
+    fn new_coordinates(
+        &self,
+        map: &Map,
+        amphibs: &[Amphipod],
+        placed: &[Amphipod],
+    ) -> Vec<(Coordinates, usize)> {
+        let mut other_positions: HashSet<Coordinates> =
+            amphibs.iter().map(|x| x.position).collect();
+        for place in placed {
+            other_positions.insert(place.position);
+        }
+        let start_from_room = matches!(map.get(&self.position).unwrap(), Room(_));
+        let mut visited: HashSet<Coordinates> = HashSet::new();
+        let mut positions: Vec<(Coordinates, usize)> = vec![(self.position, 0)];
+        let neighbors = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+        let mut candidates: Vec<(Coordinates, usize)> = vec![];
+
+        let mut found_room = false;
+
+        while !positions.is_empty() {
+            let (current_pos, steps) = positions.pop().unwrap();
+            let next_step = steps + 1;
+
+            if !visited.insert(current_pos) {
+                continue;
+            }
+
             for neighbor in neighbors.iter() {
-                let next_steps = steps + 1;
-                let next_coordinates = (
-                    (coordinates.0 as isize + neighbor.0) as usize,
-                    (coordinates.1 as isize + neighbor.1) as usize,
+                let next_position = (
+                    (current_pos.0 as isize + neighbor.0) as usize,
+                    (current_pos.1 as isize + neighbor.1) as usize,
                 );
-                if visited.contains(&next_coordinates) {
+                if other_positions.contains(&next_position) || visited.contains(&next_position) {
                     continue;
                 }
-                visited.insert(next_coordinates);
-                let energy = next_steps * self.energy;
-                match map.get(&next_coordinates).unwrap() {
+                let tile = map.get(&next_position).unwrap();
+                match tile {
                     Empty => {
-                        candidates.push((next_coordinates, energy));
-                        current_coordinates.push((next_coordinates, next_steps));
+                        if !found_room && start_from_room {
+                            candidates.push((next_position, next_step * self.energy));
+                        }
+                        positions.push((next_position, next_step));
                     }
                     Forbidden => {
-                        current_coordinates.push((next_coordinates, next_steps));
+                        positions.push((next_position, next_step));
                     }
-                    Room => {
-                        if self.targets[1] == next_coordinates {
-                            return Some(vec![(next_coordinates, energy)]);
-                        } else if self.targets[0] == next_coordinates {
-                            let next_tile = map.get(&self.targets[1]).unwrap();
-                            if let Amphi(x) = next_tile {
-                                if x == &self.sub_type {
-                                    return Some(vec![(next_coordinates, energy)]);
-                                }
-                            } else {
-                                current_coordinates.push((next_coordinates, next_steps));
-                            }
-                        } else {
-                            current_coordinates.push((next_coordinates, next_steps));
+                    Room(x) => {
+                        positions.push((next_position, next_step));
+                        if *x == self.species
+                            && !Amphipod::have_misplaced(next_position, self.species, map, amphibs)
+                        {
+                            found_room = true;
+                            candidates = vec![(next_position, next_step * self.energy)];
                         }
                     }
                     _ => {}
                 }
             }
         }
-        if candidates.is_empty() {
-            None
-        } else {
-            Some(candidates)
-        }
-    }
-
-    fn try_move_in(&self, map: &Map) -> Option<Vec<(Coordinates, usize)>> {
-        if let Amphi(x) = map.get(&self.targets[1]).unwrap() {
-            if x != &self.sub_type {
-                return None;
-            }
-        }
-        let neighbors = [(-1, 0), (0, -1), (1, 0), (0, 1)];
-        let mut visited: HashSet<Coordinates> = HashSet::new();
-        let mut current_coordinates = vec![(self.coordinates, 0)];
-        let mut targets_available = vec![];
-        while !current_coordinates.is_empty() {
-            let (coordinates, steps) = current_coordinates.pop().unwrap();
-            if self.targets.contains(&coordinates) {
-                targets_available.push(steps);
-            }
-            for neighbor in neighbors.iter() {
-                let next_steps = steps + 1;
-                let next_coordinates = (
-                    (coordinates.0 as isize + neighbor.0) as usize,
-                    (coordinates.1 as isize + neighbor.1) as usize,
-                );
-                if visited.contains(&next_coordinates) {
-                    continue;
-                }
-                visited.insert(next_coordinates);
-                match map.get(&next_coordinates).unwrap() {
-                    Empty | Forbidden | Room => {
-                        current_coordinates.push((next_coordinates, next_steps));
-                    }
-                    _ => {}
-                }
-            }
-        }
-
-        if targets_available.len() == 2 {
-            let energy = targets_available[1] * self.energy;
-            Some(vec![(self.targets[1], energy)])
-        } else if targets_available.len() == 1 {
-            let energy = targets_available[0] * self.energy;
-            if let Amphi(x) = map.get(&self.targets[1]).unwrap() {
-                if x == &self.sub_type {
-                    Some(vec![(self.targets[0], energy)])
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        } else {
-            None
-        }
-    }
-
-    fn move_amphi(&self, map: &Map) -> Option<Vec<(Coordinates, usize)>> {
-        if self.tile_type == Room {
-            self.try_move_out(map)
-        } else {
-            self.try_move_in(map)
-        }
+        candidates
     }
 }
 
-fn get_input() -> (Map, Vec<Amphipod>) {
-    let mut amphis = vec![];
-    let mut map: Map = Map::new();
-    for (y, lines) in INPUT.trim().lines().enumerate() {
-        for (x, tile_char) in lines.chars().enumerate() {
-            let tile = match tile_char {
+fn get_input(insert: Option<&str>) -> (Vec<Amphipod>, Map) {
+    let mut amphibs: Vec<Amphipod> = vec![];
+    let input = if let Some(additional_part) = insert {
+        let mut lines = INPUT.trim().lines();
+        let first_part: String = lines.by_ref().take(3).collect::<Vec<_>>().join("\n");
+        let second_part: String = lines.collect::<Vec<_>>().join("\n");
+        format!("{}\n{}\n{}", first_part, additional_part, second_part)
+    } else {
+        String::from(INPUT)
+    };
+
+    let mut map: Map = HashMap::new();
+    let mut id = 0;
+    for (y, line) in input.trim().lines().enumerate() {
+        for (x, tile) in line.chars().enumerate() {
+            let tile = match tile {
                 '#' => Wall,
+                ' ' => Void,
                 '.' => Empty,
-                'A' | 'B' | 'C' | 'D' => {
-                    amphis.push(Amphipod::new(tile_char, (x, y)));
-                    Amphi(tile_char)
+                _ => {
+                    amphibs.push(Amphipod::new((x, y), tile, id));
+                    id += 1;
+                    Room(match x {
+                        3 => 'A',
+                        5 => 'B',
+                        7 => 'C',
+                        9 => 'D',
+                        _ => unreachable!(),
+                    })
                 }
-                _ => Void,
             };
             map.insert((x, y), tile);
         }
@@ -225,122 +180,107 @@ fn get_input() -> (Map, Vec<Amphipod>) {
     map.insert((5, 1), Forbidden);
     map.insert((7, 1), Forbidden);
     map.insert((9, 1), Forbidden);
-
-    (map, amphis)
+    (amphibs, map)
 }
 
 #[derive(Debug, PartialEq, Eq)]
 struct State {
     energy_spent: usize,
-    current_amphi: Vec<Amphipod>,
-    current_map: Map,
-    moves: usize,
-    move_list: Vec<(Coordinates, Coordinates)>,
-    energy_list: Vec<usize>,
+    unplaced: Vec<Amphipod>,
+    placed: Vec<Amphipod>,
     signature: String,
 }
 
 impl State {
     fn new(
+        ampibs: Vec<Amphipod>,
+        mut already_placed: Vec<Amphipod>,
         energy_spent: usize,
-        current_amphi: Vec<Amphipod>,
-        current_map: Map,
-        moves: usize,
-    ) -> Self {
-        let signature = current_amphi.iter().fold(String::new(), |acc, amphi| {
-            format!("{}{}:{:?}", acc, amphi.sub_type, amphi.coordinates)
+        map: &Map,
+    ) -> State {
+        let (mut unplaced, mut placed): (Vec<Amphipod>, Vec<Amphipod>) = ampibs
+            .iter()
+            .cloned()
+            .partition(|x| x.need_move(map, &ampibs));
+        placed.append(&mut already_placed);
+
+        unplaced.sort_unstable_by(|x, y| {
+            if x.position.0 == y.position.0 {
+                x.position.1.cmp(&y.position.1)
+            } else {
+                x.position.0.cmp(&y.position.0)
+            }
         });
+
+        let signature = unplaced
+            .iter()
+            .map(|x| format!("{}:{:?}", x.species, x.position))
+            .collect();
+
         State {
+            unplaced,
+            placed,
             energy_spent,
-            current_amphi: current_amphi
-                .into_iter()
-                .filter(|x| x.get_move(&current_map))
-                .collect(),
-            current_map,
-            moves,
             signature,
-            energy_list: vec![],
-            move_list: vec![],
         }
     }
 }
 
-use std::cmp::Ordering;
-
 impl PartialOrd for State {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        if self.energy_spent == other.energy_spent {
-            other.moves.partial_cmp(&self.moves)
-        } else {
-            other.energy_spent.partial_cmp(&self.energy_spent)
-        }
+        other.energy_spent.partial_cmp(&self.energy_spent)
     }
 }
 
 impl Ord for State {
     fn cmp(&self, other: &Self) -> Ordering {
-        if self.energy_spent == other.energy_spent {
-            other.moves.cmp(&self.moves)
-        } else {
-            other.energy_spent.cmp(&self.energy_spent)
-        }
+        other.energy_spent.cmp(&self.energy_spent)
     }
 }
 
 pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
-    use std::collections::BinaryHeap;
-    let (map, amphipod) = get_input();
-    let mut available_states: BinaryHeap<State> = BinaryHeap::new();
-    let mut covered_steps: HashMap<String, usize> = HashMap::new();
-    let mut total_energy = usize::MAX;
+    let (amphibs, map) = get_input(None);
+    let mut heap: BinaryHeap<State> = BinaryHeap::new();
+    let mut min_erergy = 0;
 
-    available_states.push(State::new(0, amphipod, map, 0));
+    let mut visited_states: HashSet<String> = HashSet::new();
 
-    while !available_states.is_empty() {
-        let state = available_states.pop().unwrap();
-        if state.current_amphi.is_empty() {
-            total_energy = total_energy.min(state.energy_spent);
+    heap.push(State::new(amphibs, vec![], 0, &map));
+
+    while !heap.is_empty() {
+        let state = heap.pop().unwrap();
+        if state.unplaced.is_empty() {
+            min_erergy = state.energy_spent;
             break;
         }
 
-        for (i, unplaced_amphi) in state.current_amphi.iter().enumerate() {
-            if let Some(candidates_posi) = unplaced_amphi.move_amphi(&state.current_map) {
-                for (next_coordinates, energy_spent) in candidates_posi {
-                    let mut amphi = Amphipod::new(unplaced_amphi.sub_type, next_coordinates);
-                    amphi.tile_type = *state.current_map.get(&amphi.coordinates).unwrap();
-                    let mut next_amphis = state.current_amphi.clone();
-                    let mut next_map = state.current_map.clone();
-                    next_map.insert(unplaced_amphi.coordinates, unplaced_amphi.tile_type);
-                    next_map.insert(amphi.coordinates, Amphi(amphi.sub_type));
-                    next_amphis[i] = amphi;
-                    let mut next_state = State::new(
-                        state.energy_spent + energy_spent,
-                        next_amphis,
-                        next_map,
-                        state.moves + 1,
-                    );
-                    next_state.energy_list = state.energy_list.clone();
-                    next_state.move_list = state.move_list.clone();
-                    next_state.energy_list.push(energy_spent);
-                    next_state
-                        .move_list
-                        .push((unplaced_amphi.coordinates, next_coordinates));
-                    if let Some(energy) = covered_steps.get(&next_state.signature) {
-                        if *energy < next_state.energy_spent {
-                            continue;
-                        }
-                    }
-                    covered_steps.insert(next_state.signature.clone(), next_state.energy_spent);
-                    available_states.push(next_state);
-                }
+        if !visited_states.insert(state.signature) {
+            continue;
+        }
+
+        for (i, amphib) in state.unplaced.iter().enumerate() {
+            let movements = amphib.new_coordinates(&map, &state.unplaced, &state.placed);
+            for (coordinates, energy_spent) in movements {
+                let mut next_ampbibs = state.unplaced.clone();
+                next_ampbibs[i].position = coordinates;
+                let next_state = State::new(
+                    next_ampbibs,
+                    state.placed.clone(),
+                    state.energy_spent + energy_spent,
+                    &map,
+                );
+                heap.push(next_state);
             }
         }
     }
-    println!("Minimum energy used: {}", total_energy);
 
+    println!("{}", min_erergy);
     Ok(())
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    let add_on = r"  #D#C#B#A#
+  #D#B#A#C#";
+    get_input(Some(add_on));
     Ok(())
 }
